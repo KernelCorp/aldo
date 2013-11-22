9de0abfc43f8082e43ed0ebe38fed7379075aac4
max-konin <mk@kernel-corp.com>
2013-11-22 14:04:16 +0700

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aldo::Application.config.secret_token = '66384e00bfb0cec176329f15cb13efc435878307e839eb385564e69736a752ef46eee887f4a08c5fc8eaa50967bd55dd4cb672e9c9dce1e69fbc0523d9cec6b5'
