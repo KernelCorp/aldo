14d07ab5ababd36f3af93834557e3c8c2205b505
Maganov <vonagam@gmail.com>
2013-11-27 19:55:26 +0700

    add friendly gem and models for galleries

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'cancan'
 gem 'activeadmin'
 gem 'russian'
 
+gem "friendly_id", "~> 4.0.10"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131127085703) do
+ActiveRecord::Schema.define(:version => 20131127124107) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(:version => 20131127085703) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "galeries", :force => true do |t|
+    t.string   "type"
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "images", :force => true do |t|
     t.string   "path_file_name"
