dbeb0ecfa31107f3ee885eb4f5d65f4c7297ee57
Maganov <vonagam@gmail.com>
2013-12-04 15:49:39 +0700

    active_admin: remove filters #aldo-17 fixed

--- app/admin/master.rb
+++ app/admin/master.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Master do
+  config.filters = false
+  
   form do |f|
     f.inputs do
       f.input :fio
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
+# encoding: UTF-8
+
 Gallery.create title: 'искусство красоты'
 Gallery.create title: 'интерьер'
