848724087812cad1da7bc860d928cbaa00c04a67
Maganov <vonagam@gmail.com>
2013-12-04 17:34:12 +0700

    admin localization finished

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Aldo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
