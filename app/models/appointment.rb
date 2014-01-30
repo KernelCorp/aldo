6ebd9d5bc34b5e396c0a0215e347d3b5bc7fc1d4
Dmitry Maganov <vonagam@gmail.com>
2014-01-30 16:49:29 +0700

    validation for appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,9 @@
 class Appointment < ActiveRecord::Base
-  attr_accessible :email, :fio, :phone
+  attr_accessible :fio, :phone, :email
   belongs_to :service
   belongs_to :master
+
+  validates :service, :fio, :phone, presence: true
 
   after_create do
     AppointmentMailer.appointment_email(self).deliver
